04e18720249ed18f171afde49cc5c9e6148b4722
Devesh Kumar <ur.devesh@gmail.com>
2013-10-14 16:02:31 +0530

    Getting the Indexes' list on Collection
    While viewing the detail report we will show the indexes available on
    the collection. It will help to know what index may be missing.

--- ProfilerAnalyser.php
+++ ProfilerAnalyser.php
@@ -1,7 +1,22 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+            if(isset($_GET['type']) && strcasecmp($_GET['type'], 'idx') == 0)
+            {
+                $dbName = $_GET['dbName'];
+                $mongoConn = $_GET['ipAddress'] . ':' . $_GET['port'];
+                $ns = $_GET['ns'];
+                $mongoConn = new MongoClient($mongoConn);
+                $db = $mongoConn->$dbName;
+                $indexesCollection = new MongoCollection($db, 'system.indexes');
+                $query = array('ns' => $ns);
+                $result = $indexesCollection->find($query);
+                $nsindexes = array();
+                foreach ($result as $doc) {
+                    $nsindexes[] = $doc;
+                }
+                echo json_encode($nsindexes);
+                exit;
+            }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -26,12 +41,45 @@ and open the template in the editor.
         </style>
         <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.10.2.min.js"></script>
         <script type="text/javascript">
-            $(document).ready(function() {
-                $(".summaryRow").on("click", function(event) {
-                    $(this).next(".DetailRow").toggle();
-                    event.preventDefault();
+        $(document).ready(function () {
+        $(".summaryRow").on("click", function (event) {
+        var detailRow = $(this).next(".DetailRow");
+        var dbName = $("#dbName").val();
+        var port = $("#port").val();
+        var ipAddress = $("#ipAddress").val();
+        var ns = $(this).find('td:eq(3)').text();
+        $.ajax({
+            url: 'ProfilerAnalyser.php',
+            type: 'GET',
+            data: {
+                type: 'idx',
+                dbName: dbName,
+                port: port,
+                ipAddress: ipAddress,
+                ns: ns
+            },
+            dataType: 'json',
+            success: function (result) {
+                var idxDetail = '';
+                $.each(result, function (counter, item) {
+                    idxDetail += ' Key Name : ' + item.name;
+                    idxDetail += ' V  : ' + item.v;
+                    idxDetail += ' col  : ';
+                    $.each(item.key, function (key, value) {
+                        idxDetail += key + ' : ' + value;
+                    });
                 });
-            });
+
+                detailRow.toggle();
+                alert(idxDetail);
+                event.preventDefault();
+            },
+            error: function (error) {
+                alert('Some error' + error);
+            }
+        });
+    });
+});
         </script>
     </head>
     <body>
@@ -45,57 +93,76 @@ and open the template in the editor.
                     <label for="port">Port</label><input type="text" id="port" name="port" value="27017"/>               
                 </p>
                 <p>
-                    <label for="dbName">Database Name</label> <input type="text" name="dbName"/>
+                    <label for="dbName">Database Name</label> 
+                    <?php
+                    if (isset($_POST['dbName'])) {
+                        echo '<input type="text" name="dbName" id="dbName" value="' . $_POST['dbName'] . '"/>';
+                    } else {
+                        echo '<input type="text" name="dbName" id="dbName" value=""/>';
+                    }
+                    ?>
+
                 </p>
                 <p>
 
                     <input type="submit" value="Submit" name="btnSubmit"/>
                 </p>
-
+                 <input type="hidden" name="currentsortOrder" id="currentsortOrder" value="<?php echo(isset($_POST['currentsortOrder']) ? (($_POST['currentsortOrder'] == 'asc') ? 'desc' : 'asc') :  'asc') ?>"/>
             </div>
-        </form>
-        <?php
-        // put your code here
-        if (isset($_POST['btnSubmit'])) {
-            $mongoConn = $_POST['ipAddress'] . ':' . $_POST['port'];
-            $dbName = $_POST['dbName'];
-            $mongoConn = new MongoClient($mongoConn);
-            $db = $mongoConn->$dbName;
-            $profileCollection = new MongoCollection($db, 'system.profile');
-            $counter = 0;
-            echo '<table style="width:100%"><thead><tr><th>S.No</th><th>Time</th><th>Operation</th><th>Collection</th><th>Date</th></tr></thead>';
-            foreach ($profileCollection->find()->sort(array('millis' => -1)) as $p) {
-                $counter++;
-                echo '<tr class="summaryRow">';
-                echo '<td>' . $counter . '</td>';
-                echo '<td>' . $p['millis'] . '</td>';
-                echo '<td>' . $p['op'] . '</td>';
-                echo '<td>' . $p['ns'] . '</td>';
-                echo'<td>' . date("d-M-y", $p['ts']->sec) . '</td>';
-                echo '</tr>';
-                echo '<tr class="DetailRow">';
-                echo '<td colspan=5>';
-                if (array_key_exists('nscanned', $p)) {
-                    echo 'Document Scanned : ' . $p['nscanned'] . ' and total returned ' . $p['nreturned'] . '<br>';
-                }
-                if (array_key_exists('moved', $p)) {
-                    echo 'Document moved : ' . $p['moved'] . ' and total doc moved :' . $p['nmoved'] . '<br>';
-                }
-                if (array_key_exists('updateobj', $p)) {
-                    echo '<p> ' . var_dump($p['updateobj']) . '</p>';
-                }
-                if (array_key_exists('query', $p)) {
-                    echo '<p style="font-size:12px">';
-                    var_dump($p['query']);
-                    echo '</p>';
-                }
-                echo '</td></tr>';
-            }
-            echo '</table>';
-            if ($counter == 0)
-                echo ' No records found !';
-        }
-        ?>
 
+            <?php
+            if (isset($_POST['btnSubmit']) || isset($_POST['btnSort'])) {
+                $mongoConn = $_POST['ipAddress'] . ':' . $_POST['port'];
+                $dbName = $_POST['dbName'];
+                $mongoConn = new MongoClient($mongoConn);
+                $db = $mongoConn->$dbName;
+                $profileCollection = new MongoCollection($db, 'system.profile');
+                $counter = 0;
+                echo '<div>';
+                echo '<select id="sortBy" name="sortBy">';
+                echo '<option value="millis">Time</option>';
+                echo '<option value="op">Operation</option>';
+                echo '<option value="ns">Collection</option>';
+                echo '</select>';
+                echo '<input type="submit" value="Sort" name="btnSort" id="btnSort" />';
+                echo '<table style="width:100%"><thead><tr><th>S.No</th><th>Time</th><th>Operation</th><th>Collection</th><th>Date</th></tr></thead>';
+          
+                $sortOrder = $_POST['currentsortOrder'] == "asc" ? 1 : -1;
+                
+                $sorting = isset($_POST['sortBy']) ? array($_POST['sortBy'] => $sortOrder) : array('millis' => $sortOrder);
+                foreach ($profileCollection->find()->sort($sorting) as $p) {
+                    $counter++;
+                    echo '<tr class="summaryRow">';
+                    echo '<td>' . $counter . '</td>';
+                    echo '<td>' . $p['millis'] . '</td>';
+                    echo '<td>' . $p['op'] . '</td>';
+                    echo '<td>' . $p['ns'] . '</td>';
+                    echo'<td>' . date("d-M-y", $p['ts']->sec) . '</td>';
+                    echo '</tr>';
+                    echo '<tr class="DetailRow">';
+                    echo '<td colspan=5>';
+                    if (array_key_exists('nscanned', $p)) {
+                        echo 'Document Scanned : ' . $p['nscanned'] . ' and total returned ' . $p['nreturned'] . '<br>';
+                    }
+                    if (array_key_exists('moved', $p)) {
+                        echo 'Document moved : ' . $p['moved'] . ' and total doc moved :' . $p['nmoved'] . '<br>';
+                    }
+                    if (array_key_exists('updateobj', $p)) {
+                        echo '<p> ' . var_dump($p['updateobj']) . '</p>';
+                    }
+                    if (array_key_exists('query', $p)) {
+                        echo '<p style="font-size:12px">';
+                        var_dump($p['query']);
+                        echo '</p>';
+                    }
+                    echo '</td></tr>';
+                }
+                echo '</table>';
+                if ($counter == 0)
+                    echo ' No records found !';
+            }
+            ?>
+            
+        </form>
     </body>
 </html>
